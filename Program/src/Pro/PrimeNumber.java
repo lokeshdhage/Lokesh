5323ef5870bca2260051fb84037258e6e6c026ae
LOKESH DK <LOKESH DK@LAPTOP-B9RH7SGE>
2022-05-14 10:04:00 +0530

    g

--- Program/src/Pro/PrimeNumber.java
+++ Program/src/Pro/PrimeNumber.java
@@ -1,6 +1,35 @@
 package Pro;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
+	
+	public static void main(String[] args) {
+		
+		int n,Count=0;
+		System.out.println("Enter any number");
+		
+		Scanner r = new Scanner(System.in);
+		n = r.nextInt();
+		
+		for(int i = 1; i <= n; i++)
+		{
+		    if(n%i==0)
+		    {
+		    	Count++;
+		    }
+		}
+		if(Count==2)
+		{
+			System.out.println("Prime Number");
+		}
+		else
+		{
+			System.out.println("Not Prime Number");
+		}
+	}
+	
+	
 	
 
 }
