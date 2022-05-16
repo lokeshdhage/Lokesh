c05c287a15b64bc0c155e6497c796b50dd1f3d04
LOKESH DK <LOKESH DK@LAPTOP-B9RH7SGE>
2022-05-16 17:40:54 +0530

    Pop up and mouse action cod

--- Program/src/Pro/MouseAction.java
+++ Program/src/Pro/MouseAction.java
@@ -1,6 +1,7 @@
 package Pro;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,12 +22,20 @@ public class MouseAction {
 		
 		WebElement order = driver.findElement(By.xpath("//span[text() = 'Your Orders']"));
 		
-		Actions a = new Actions(driver);
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		
+		js.executeScript("window.scrollBy(0,1800)"); // scroll down
+		Thread.sleep(3000);
+		js.executeScript("window.scrollBy(0,-1800)"); // scroll up
+		// js.executeScript("window.scrollBy(1800,0)");  // scroll right
+		// js.executeScript("window.scrollBy(-1800,0)");  // scroll left
+		
+		//Actions a = new Actions(driver);
 		
 	//	a.moveToElement(as).perform();
 	//	a.moveToElement(order).click().build().perform();
 		
-		a.moveToElement(as).moveToElement(order).click().build().perform();
+		//a.moveToElement(as).moveToElement(order).click().build().perform();
 		
 		//a.click(); // Left click
 		//a.doubleClick(); // Left double click
---
+++ Program/src/Pro/PopUp.java
@@ -0,0 +1,37 @@
+package Pro;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PopUp {
+	
+	public static void main(String [] args)
+	{
+		System.setProperty("webdriver.chrome.driver" , "C:\\Users\\rocks\\Downloads\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+	    driver.get("https://demo.guru99.com/test/delete_customer.php");
+	    
+	    WebElement submit = driver.findElement(By.xpath("//input[@name = 'submit']")); 
+	    
+	    submit.click();
+	    
+	    Alert al = driver.switchTo().alert();
+	    
+	    String text = al.getText();
+	    System.out.println(text);
+	    
+	     al.accept(); // to click on OK, YES, Continue, Accept. Submit
+	    // Alert al1 = driver.switchTo().alert();
+	     //al1.accept();
+	  
+//	    al.dismiss(); // to click on No, Cancel, Dismiss
+//	    al.getText(); // to get text present on alert popup
+//	    al.sendKeys("hi"); // to send data/text on alert popup 
+	}
+
+}
